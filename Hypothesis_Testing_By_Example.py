0011db15ffbeeef247c01a4e4aff60c90279d1e1
Wajdi Ben Saad <6123092+WajdiBenSaad@users.noreply.github.com>
2021-02-25 20:28:59 +0100

    Add second question

--- Hypothesis_Testing_By_Example.py
+++ Hypothesis_Testing_By_Example.py
@@ -28,3 +28,50 @@ from statsmodels.stats.proportion import proportions_ztest
 
 stat, p_value = proportions_ztest(count=len(heart[heart['target']==0]), 
                                   nobs=len(heart['target']), value=0.42, alternative='larger')
+
+#### Question 2 ########
+########################
+'''
+test if the population proportion of females with heart disease is different 
+from the population proportion of males with heart disease.
+============================================================
+
+we're going to test the difference of proportion in two populations
+our hypothesis to be checked are:
+    
+ h0:   p_male = p_female
+ ha:   p_male <> p_female
+
+We will use a 2-sample z-test to check if the sample allows us to accept or reject the null hypothesis
+
+'''
+
+# spliting the data to 2 dataframes:
+    
+females=heart.loc[heart['sex']==1, ['target']]
+
+males=heart.loc[heart['sex']==0,['target']]
+
+## calculating the proportions of each group:
+    
+p_female= len(females[females['target']==1])/len(females)
+
+p_male= len(males[males['target']==1])/len(males)
+
+
+gender_test=pd.DataFrame( {
+    'count':[males['target'].sum(), females['target'].sum()],
+    'nobs':[males['target'].count(), females['target'].count()]
+    }
+    ,index=['males','females']
+    )
+
+proportions_ztest(gender_test['count'], gender_test['nobs'])
+
+'''
+the p value of our test is: 1.0071642033238824e-06, we can't reject the null hypothesis
+The population proportion of males with heart disease is not significantly different than
+the population proportion of females with heart disease.
+
+
+'''
