bdd2157ea0220271cdb681c7f81691719108fb11
Wajdi Ben Saad <6123092+WajdiBenSaad@users.noreply.github.com>
2021-02-25 20:28:22 +0100

    Add first question

--- Hypothesis_Testing_By_Example.py
+++ Hypothesis_Testing_By_Example.py
@@ -1,1 +1,30 @@
 
+#### Question 1 ########
+########################
+'''
+The population proportion of Ireland having heart disease is 42%.
+Are more people suffering from heart disease in the US”?
+============================================================
+
+ '''
+ 
+import pandas as pd
+ 
+heart=pd.read_table('heart.csv',sep=',')
+
+'''
+Ho: p = 0.42  #null hypothesis
+H1: p > 0.42  #alternative hypothesis
+'''
+
+p_us=len(heart[heart['target']==0])/  len(heart['target'])
+
+'''
+population proportion having heart disease in the US is 46%
+
+'''
+from statsmodels.stats.proportion import proportions_ztest
+
+
+stat, p_value = proportions_ztest(count=len(heart[heart['target']==0]), 
+                                  nobs=len(heart['target']), value=0.42, alternative='larger')
