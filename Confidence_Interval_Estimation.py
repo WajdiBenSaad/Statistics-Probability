3be27f0d1b3333a9bb79ca99ccc2ccc43639a071
Wajdi Ben Saad <6123092+WajdiBenSaad@users.noreply.github.com>
2021-02-26 08:37:12 +0100

    Add second part of CI estimation examples

--- Confidence_Interval_Estimation.py
+++ Confidence_Interval_Estimation.py
@@ -70,3 +70,84 @@ import statsmodels.api as sm
 sm.stats.proportion_confint(n * prop_females, n)
 
 # ==> same conf interval ! :D
+
+'''
+CI for the Difference in Population Proportion
+===============================================
+Is the population proportion of females with heart disease 
+the same as the population proportion of males with heart disease? 
+If they are the same, then the difference in both 
+the population proportions will be zero.
+
+==>
+We will calculate a confidence interval of the difference 
+in the population proportion of females and males with heart disease.
+'''
+
+
+p_male = 114/(114+93)  #male population proportion
+n = 114+93             #total male population
+
+#Calculate the standard error for the male population proportion
+se_male = np.sqrt(p_male * (1 - p_male) / n)
+
+#calculate the difference in the standard error of male and female population with heart disease
+se_diff = np.sqrt(se_female**2 + se_male**2)
+
+'''
+Use this standard error to calculate the difference 
+in the population proportion of males and females with heart disease 
+and construct the CI of the difference.
+'''
+d = 0.55 - 0.24
+lcb = d - 1.96 * se_diff  #lower limit of the CI
+ucb = d + 1.96 * se_diff  #upper limit of the CI
+
+# ==> our CI is [0.2 , 0.41]
+'''
+The CI is 0.18 and 0.4. This range does not have 0 in it. 
+Both the numbers are above zero.
+We cannot make any conclusion that the population proportion 
+of females with heart disease is the same as the population 
+proportion of males with heart disease. 
+If the CI would be -0.12 and 0.1, we could say that the male and 
+female population proportion with heart disease is the same.
+'''
+
+
+'''
+Calculation of CI of mean
+calculate the confidence interval of the mean cholesterol 
+level of the female population
+==========================
+Let’s find the mean, standard deviation, and population size 
+for the female population
+'''
+
+
+h.groupby("sex").agg({"chol": [np.mean, np.std, np.size]})
+
+mean_fe = 261.75  #mean cholesterol of female
+sd = 64.9         #standard deviation for female population
+n = 97            #Total number of female
+z = 1.96          #z-score from the z table mentioned before
+
+#Here 1.96 is the z-score for a 95% confidence level.
+#Calculate the standard error using the formula 
+#for the standard error of the mean
+
+se = sd /np.sqrt(n)
+
+#Construct the CI
+
+lcb = mean_fe - z* se  #lower limit of the CI
+ucb = mean_fe + z* se  #upper limit of the CI
+(lcb, ucb)
+'''
+The CI came out to be 248.83 and 274.67.
+That means the true mean of the cholesterol of the female population
+will fall between 248.83 and 274.67
+
+
+
+'''
